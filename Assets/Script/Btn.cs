17cbdda4dfda5ab9fb732e2c51fdc9fec79c4a28
tancredegorand <119679491+tancredegorand@users.noreply.github.com>
2024-11-28 21:30:03 -0500

    axe

--- Assets/Script/Btn.cs
+++ Assets/Script/Btn.cs
@@ -1,3 +1,42 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.XR.Interaction.Toolkit;
+
+// public class Btn : MonoBehaviour
+// {
+//     private XRSimpleInteractable interactable;
+//     private Vector3 newPosition = new Vector3(71.22f, 22.88f, 42.85f);
+//     private Vector3 originalPosition = new Vector3(71.22f, 22.89f, 42.85f);
+
+//     void Start()
+//     {
+//         interactable = GetComponent<XRSimpleInteractable>();
+        
+
+//         interactable.selectEntered.AddListener(OnSelect);
+//         interactable.selectExited.AddListener(OnDeselect);
+//     }
+
+//     private void OnSelect(SelectEnterEventArgs args)
+//     {
+  
+//         transform.position = newPosition;
+//     }
+
+//     private void OnDeselect(SelectExitEventArgs args)
+//     {
+
+//         transform.position = originalPosition;
+//     }
+
+//     void Update()
+//     {
+//         Debug.Log(transform.position);
+//     }
+// }
+
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,32 +45,51 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class Btn : MonoBehaviour
 {
     private XRSimpleInteractable interactable;
-    private Vector3 newPosition = new Vector3(71.22f, 22.88f, 42.85f);
-    private Vector3 originalPosition = new Vector3(71.22f, 22.89f, 42.85f);
+
+
+    private Vector3 prefabPosition = new Vector3(71.295f, 22.819f, 42.213f);
+
+
+    [SerializeField] private GameObject axeInstantiate;
+
+
+    private Vector3 newPositionBtn = new Vector3(71.22f, 22.88f, 42.85f);
+    private Vector3 originalPositionBtn = new Vector3(71.22f, 22.89f, 42.85f);
 
     void Start()
     {
         interactable = GetComponent<XRSimpleInteractable>();
         
-        // Ajoute des écouteurs pour les événements selectEntered et selectExited
         interactable.selectEntered.AddListener(OnSelect);
         interactable.selectExited.AddListener(OnDeselect);
     }
 
     private void OnSelect(SelectEnterEventArgs args)
     {
-        // Déplace l'objet à la nouvelle position
-        transform.position = newPosition;
+   
+        transform.position = newPositionBtn;
+
+        
+        if (axeInstantiate != null)
+        {
+            Debug.Log("creat");
+            Instantiate(axeInstantiate, prefabPosition, Quaternion.identity);
+        }
+        else
+        {
+            Debug.LogWarning("Prefab à instancier non assigné !");
+        }
     }
 
     private void OnDeselect(SelectExitEventArgs args)
     {
-        // Restaure la position originale
-        transform.position = originalPosition;
+
+        transform.position = originalPositionBtn;
     }
 
     void Update()
     {
-        Debug.Log(transform.position);
+        //Debug.Log(transform.position);
+        
     }
 }
