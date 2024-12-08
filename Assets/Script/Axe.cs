d0b6a69debbd8a16631489cee8fe5dff86f30077
tancredegorand <119679491+tancredegorand@users.noreply.github.com>
2024-12-07 19:35:30 -0500

    lancer

--- Assets/Script/Axe.cs
+++ Assets/Script/Axe.cs
@@ -1,43 +1,18 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-// public class Axe : MonoBehaviour
-// {
-//     private Rigidbody axeRigidbody;
-//     private bool isTouchingTarget = false;
+public class Axe : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
 
-//     void Start()
-//     {
-//         axeRigidbody = GetComponent<Rigidbody>();
-//     }
-
-//     void Update()
-//     {
-//         if (!isTouchingTarget)
-//         {
-//             axeRigidbody.useGravity = true;
-//         }
-//     }
-
-//     private void OnTriggerEnter(Collider other)
-//     {
-//         if (other.CompareTag("Target"))
-//         {
-//             isTouchingTarget = true; 
-//             axeRigidbody.useGravity = false;
-//             axeRigidbody.velocity = Vector3.zero;
-//             axeRigidbody.angularVelocity = Vector3.zero;
-//         }
-//     }
-
-//     private void OnTriggerExit(Collider other)
-//     {
-//         if (other.CompareTag("Target"))
-//         {
-//             isTouchingTarget = false;
-//         }
-//     }
-
-
-// }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
